0d048867adb0d961eb8960b05ac5f85b7e1fc996
Aiwin Charalil Manuel <aw380590@dal.ca>
2025-01-10 15:41:02 -0400

    upload dag

---
+++ Airflow_DAGs/dags/Upload_to_s3_Company_employee-details_DAG.py
@@ -0,0 +1,35 @@
+from airflow.models import DAG
+from datetime import datetime , timedelta
+from airflow.providers.amazon.aws.hooks.s3 import Hook
+from airflow.operators.python import PythonOperator
+
+
+
+aws_connection_id = 'aws_connection'
+bucket_name = 'aiwins'
+
+def uploadtos3():
+    hook = Hook(aws_conn_id=aws_connection_id)
+
+defualt_args = {
+    "owner":"aiwinmanuel",
+    "start_date":datetime(25,1,1),
+    "retires":1,
+    "retry_delay" : timedelta(minutes=1),
+    
+}
+
+with DAG (
+    "upload_to_s3",
+    default_args=defualt_args
+    tags = "S3 upload company employee details",
+    catchup= False,
+    description="uploading company employee details to s3 bucket"
+    schedule_interval=None,
+    ) as dag:
+    
+    uploadtos3 = PythonOperator(
+        task_id = "uploadtos3",
+        python_callable=uploadtos3
+    )
+    
