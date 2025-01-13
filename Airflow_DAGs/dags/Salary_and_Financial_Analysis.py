811056af638398749d9654300f92cea3fea18102
Aiwin Charalil Manuel <aw380590@dal.ca>
2025-01-13 17:13:34 -0400

    salary_added

--- Airflow_DAGs/dags/Salary_and_Financial_Analysis.py
+++ Airflow_DAGs/dags/Salary_and_Financial_Analysis.py
@@ -4,6 +4,7 @@ from airflow.operators.python import PythonOperator
 from airflow.providers.amazon.aws.hooks.s3 import S3Hook
 import pandas as pd
 import io
+import ace_tools as tools
 
 
 
@@ -13,6 +14,7 @@ aws_bucket_name = "aiwins"
 salary_history_table_key = "Company_Employee_Details/salary_history.csv"
 performance_reviews_table_key = 'Company_Employee_Details/performance_reviews.csv'
 employee_table_key = 'Company_Employee_Details/employees.csv'
+departments_table_key = 'Company_Employee_Details/departments.csv'
 
 
 
@@ -36,9 +38,13 @@ def extract_data(**kwargs):
     employee_data = hook.read_key(employee_table_key, bucket_name=aws_bucket_name)
     df_employee = pd.read_csv(io.StringIO(employee_data))
     
+    departments_data = hook.read_key(departments_table_key, bucket_name=aws_bucket_name)
+    df_departments = pd.read_csv(io.StringIO(departments_data))
+    
     kwargs['ti'].xcom_push(key='employee', value=df_employee.to_dict(orient='records'))
     kwargs['ti'].xcom_push(key='performance_reviews',value=df_performance_reviews.to_dict(orient='records'))
     kwargs['ti'].xcom_push(key='salary_history',value=df_salary_history.to_dict(orient='records'))
+    kwargs['ti'].xcom.push(key='departments', value = df_departments.to_dict(orient="records"))
     
 
 def currrency_conversion(**kwargs):
@@ -84,9 +90,17 @@ def salaryBand(**kwargs):
     
 def average_salary(**kwargs):
     ti=kwargs['ti']
-    df_employee = pd.DataFrame(ti.xcom_pull(task_id='date_formatting',key='employee'))
+    df_employee = pd.DataFrame(ti.xcom_pull(task_id = "date_formating_task", key = 'employee'))
+    df_salary_history = pd.DataFrame(ti.xcom_pull(task_id='date_formatting',key='salary_history'))
+    df_departments = pd.DataFrame(ti.xom_pull(task_id = 'extextracting_datara', key = 'departments'))
+    df_latest_salary = df_salary_history.sort_values(by=['EmployeeID', 'EffectiveDate']).groupby('EmployeeID').last().reset_index()
+    df_employee_salary = pd.merge(df_employee,df_latest_salary[['EmployeeID', 'UpdatedSalary']], on='EmployeeID', how='left' )
+    df_employee_salary= pd.merge(df_employee_salary,df_departments[['DepartmentID', 'DepartmentName']], on = 'DepartmentID', how='left' )
+    df_avg_salary = df_employee_salary.groupby(['DepartmentID', 'DepartmentName', 'PositionID'])['UpdatedSalary'].mean().reset_index()
+    df_avg_salary.rename(columns={'UpdatedSalary': 'AvgSalary'}, inplace=True)
+    tools.display_dataframe_to_user(name="Average Salary by Department & Position", dataframe=df_avg_salary)
     
-    
+        
 
 default_args = {
     "owner":"aiwinmanuel",
@@ -124,8 +138,12 @@ with DAG(
         python_callable=salaryBand
     )
     
+    average_salary_task = PythonOperator(
+        task_id = "average_salary",
+        python_callable=average_salary
+    )
     
     
     
-    extracting_task >> coverting_currency_task >> date_formatting_task >> salary_band_task
+    extracting_task >> coverting_currency_task >> date_formatting_task >> salary_band_task >> average_salary_task 
     
